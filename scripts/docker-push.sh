8955a02c35ef08f68be4fbe5ca941e21c959872b
Richard Case <richard.case@outlook.com>
2018-10-03 10:56:58 +0100

    Build changes and charts

--- scripts/docker-push.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ -z "$DOCKER_USER" ]; then
-    echo "PR build, skipping Docker Hub push";
-else
-    echo $DOCKER_PASS | docker login -u $DOCKER_USER --password-stdin;
-    make docker-push;
-fi
